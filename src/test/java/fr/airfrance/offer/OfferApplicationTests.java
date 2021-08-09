c4703f30481ff7a231150005c2081f05fd5b39b4
kerdoud <taharkerdoud@gmail.com>
2021-08-09 15:44:01 +0200

    first commit, generated project from spring boot initializer

---
+++ src/test/java/fr/airfrance/offer/OfferApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.airfrance.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
